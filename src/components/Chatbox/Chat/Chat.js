94effa711444370d5f4d8aa9c172053e81c6ae94
Audrey <audreymedaino@gmail.com>
2020-08-02 20:27:57 -0400

    Added logo

--- src/components/Chatbox/Chat/Chat.js
+++ src/components/Chatbox/Chat/Chat.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from "socket.io-client";
+import './Chat.css';
 
 const Chat = () => {
   return (
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 const Toolbar = props => (
   <>
   <header> 
     <nav>
       <div></div>
-      <div><a href="/"> THE LOGO</a></div>
+      <div><img src={logo} className="App-logo" alt="logo"/></div>
       <div>
         <ul>
           <li><a href="/search">Search</a></li>
           <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/chatbox">Chatbox</a></li>
+          <li><a href="/chatbox/join">Chatbox</a></li>
           <li><a href="/pinboard">Pinboard</a></li>
         </ul>
       </div>
