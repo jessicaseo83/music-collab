7b19aec6641d8c75fed9fea0bcb12cbba3238f45
Adel Rashed <adel_rashed@outlook.com>
2020-08-02 20:30:56 -0400

    created sign in form

---
+++ src/components/SignIn.js
@@ -0,0 +1,39 @@
+
+import React from "react";
+import  { useState } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+export default function SignIn(){
+  const [info,setInfo]=useState({email:"",password:""})
+
+  return (
+
+    <Form className="m-5"onSubmit={(event)=> {event.preventDefault();console.log(info)}} autoComplete="off">
+      <h2>Sign In</h2>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" 
+    required={true} 
+    value={info.email}
+    onChange={(event)=>setInfo({...info,email:event.target.value})}/>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" 
+    required={true} 
+    value={info.password}
+     onChange={(event)=>setInfo({...info,password:event.target.value})}/>
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+
+  )
+
+
+}
+
