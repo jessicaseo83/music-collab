e4e79fb77d97ec7ed0b103bec060d2d4b389ccb3
Jessica Seo <jessicaseo83@gmail.com>
2020-08-04 18:50:03 +0000

    add collaborate component

---
+++ src/components/Collaborate/Collaborate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from '../map/map'
+
+const Collaborate = () => {
+  return (
+    <>
+    <h1>collabo</h1>
+    <Map />
+    </>
+  )
+}
+
+export default Collaborate;
