becd290e0810fffc7c6d7b59b9fb102b32ac1b39
Jessica Seo <jessicaseo83@gmail.com>
2020-08-05 16:41:28 +0000

    Correct wrong data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 DELETE FROM users;
 
 INSERT INTO users (name, password, email, date_of_birth, city, postal_code, lat, lng, role, profile_pic)
-VALUES ('Samantha Simons', 'password', 'osimons@hello.com', '1988-09-08', 'Toronto', 'H2Y 2H6', 45.50092, -73.55984529, 'Producer', 'https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&h=350'),
-('Kelahni Kelly', 'password', 'kikeka@bye.com', '2000-12-22', 'Ottawa', 'K1A 0A5', 45.420413, 45.420413, 'Sound Engineer', 'https://images.pexels.com/photos/2123778/pexels-photo-2123778.jpeg?auto=compress&cs=tinysrgb&h=350'),
+VALUES ('Samantha Simons', 'password', 'osimons@hello.com', '1988-09-08', 'Montreal', 'H2Y 2H6', 45.50092, -73.55984529, 'Producer', 'https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&h=350'),
+('Kelahni Kelly', 'password', 'kikeka@bye.com', '2000-12-22', 'Ottawa', 'K1A 0A5', 45.420413, -75.7024501, 'Sound Engineer', 'https://images.pexels.com/photos/2123778/pexels-photo-2123778.jpeg?auto=compress&cs=tinysrgb&h=350'),
 ('Freddie Jackson', 'password', 'rockstar@great.net', '1981-05-02', 'Vancouver', 'V5K 1A4', 49.291308099, -123.055468, 'Musician', 'https://images.pexels.com/photos/935969/pexels-photo-935969.jpeg?auto=compress&cs=tinysrgb&h=350'),
-('Mila Jay', 'password', 'miyako@music.com', '1994-02-03', 'Ottawa', 'K0A 2Z0', 45.1496224, -75.80888139999999, 'Songwriter', 'https://images.pexels.com/photos/1078058/pexels-photo-1078058.jpeg?auto=compress&cs=tinysrgb&h=350'),
+('Mila Jay', 'password', 'miyako@music.com', '1994-02-03', 'Ottawa', 'K1B 1B7', 45.44136109999999, -75.5748364, 'Songwriter', 'https://images.pexels.com/photos/1078058/pexels-photo-1078058.jpeg?auto=compress&cs=tinysrgb&h=350'),
 ('Jay Park', 'password', 'jparkrighthere@aomgg.com', '1999-10-10', 'Montreal', 'H1A 5G8', 45.6522394, -73.50247949999999, 'Composer', 'https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&h=350'),
 ('Hannibal Lecter', 'password', 'creepyguy@hahaha.com', '2003-01-01', 'Vancouver', 'V5K 1C4', 49.2910115, -123.0475533, 'Producer', 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&h=350'),
 ('Avril Lavigne', 'password', 'ramonalavigne@canada.com', '1984-03-27', 'Toronto', 'M4B 1B5', 43.70144519999999, -79.2935596, 'Musician', 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&h=350'),
