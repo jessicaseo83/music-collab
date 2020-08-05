f00cf5ee623a464c8c3b278158e0a5a5c4633c21
Audrey <audreymedaino@gmail.com>
2020-08-04 22:32:50 -0400

    Added some code for the list view; :

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import {useState,useEffect} from 'react';
 import axios from 'axios'
+import "./Search.css"
 
-const Search = () => {
-  const [users,setUsers] = useState();
+
+const Search = (props) => {
+  const [users,setUsers] = useState([]);
   useEffect(()=>{
     axios.get('/users')
     .then(res => setUsers(res.data))
 
   },[])
-  return (
-    <h1>Search</h1>
-  )
+
+  // return (
+  //   userDetail = (
+  //     <div>
+  //       {this.props.users.map(user => (
+  //         <User
+  //           key={user.id}
+  //           name={user.name}
+  //           email={user.email}
+  //           profile_pic={user.profile_pic}
+  //           />
+  //       ))}
+  //     </div>
+  //   )
+  // )
+  //       }
+
+return (
+<>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<div className="names">{users.map(user => <div>{user.name}</div>)}</div>
+<div className="emails">{users.map(user => <div>{user.email}</div>)}</div>
+<div className="age">{users.map(user => <div>{user.city}</div>)}</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <img src={users.map(user => <div>{user.profile_pic}</div>)} alt="" class="img-rounded img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <h4>{users.map(user => <div>{user.name}</div>)}</h4>
+                        <small><cite title="San Francisco, USA">San Francisco, USA <i class="glyphicon glyphicon-map-marker">
+                        </i></cite></small>
+                        <p>
+                            <i class="glyphicon glyphicon-envelope"></i>{users.map(user => <div>{user.email}</div>)}
+                            <br />
+                            <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
+                            <br />
+                            <i class="glyphicon glyphicon-gift"></i>June 02, 1988</p>
+                
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">
+                                Social</button>
+                            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span><span class="sr-only">Social</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="https://plus.google.com/+Jquery2dotnet/posts">Google +</a></li>
+                                <li><a href="https://www.facebook.com/jquery2dotnet">Facebook</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Github</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</>
+)
 }
+
 
 export default Search;
