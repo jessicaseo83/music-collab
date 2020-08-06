01807096ad624c1728ac957b27bda0fa7124dcdc
Audrey <audreymedaino@gmail.com>
2020-08-06 13:11:30 -0400

    Pinboard

--- src/components/Pinboard/Pinboard.js
+++ src/components/Pinboard/Pinboard.js
@@ -1,29 +1,62 @@
- 
-import React, { useState } from 'react';
-import ReactStickies from 'react-stickies'; //ES6
+import React from 'react';
+import {useState,useEffect} from 'react';
+import axios from 'axios'
 
-function Pinboard (props) {
-      const [note, setNote] = useState ([])
+
+import "./Pinboard.css"
+import UserInd from "./list/User.js"
+import Image from 'react-bootstrap/Image';
+
+const Search = (props) => {
+  const [users,setUsers] = useState([]);
+  const [role,setRole] = useState();
+  const [city,setCity] = useState();
+  const [filteredUsers,setFilteredUsers] = useState([]);
+  useEffect(()=>{
+    axios.get('/users')
+    .then(res => setUsers(res.data))
     
-    const onSave = ()=> {
-      // Make sure to delete the editorState before saving to backend
-      const notes = notes;
-      notes.map(note => {
-        delete note.editorState;
-      })
-      // Make service call to save notes
-      // Code goes here...
-    }
-    const onChange = (note) => {
-      setNote({ // Update the notes state
-        note
-      })
-    }
-    return (
-      <ReactStickies
-        notes={onSave}
-        onChange={onChange}
-      />
-    )
-  }
-export default Pinboard;
+    
+  },[])
+
+
+
+// if the user clicks on the city, we want to return users from that city
+// users.city === city 
+if (city) {
+  const userCity = users.filter(user => user.city === city)
+  setFilteredUsers(userCity)
+}
+
+const userList = filteredUsers.map (user => <UserInd user={user}/>)
+
+return (
+
+<>
+<label className="filter1" for="role">Select a role</label>
+  <select id="role" onChange={(event)=>setRole(event.target.value)}>
+      <option>Musician</option>
+      <option>Sound Engineer</option>
+      <option>Songwriter</option>
+</select>
+
+<label className="filter2" for="city">Select a city</label>
+<select id="city" onChange={(event)=>{event.preventDefault(); setCity(event.target.value)}}>
+      <option>Montreal</option>
+      <option>Vancouver</option>
+      <option>Ottawa</option>
+</select>
+
+  <Map users={users} />
+  {userList}
+
+
+</>
+)
+}
+
+// dynamic rendering
+// map value ==2 and list ==4 
+
+
+export default Search;
