ac7cffd73f3ec3a8150336bf01af5e33f0a159cc
Audrey <audreymedaino@gmail.com>
2020-08-06 16:42:30 -0400

    Inverted counter does not work yet

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState,useEffect} from 'react';
 import axios from 'axios'
-import Map from './map/map'
+import Map from './Map/Map'
 
 import "./Search.css"
 import UserInd from "./list/User.js"
@@ -60,3 +60,4 @@ return (
 
 
 export default Search;
+
