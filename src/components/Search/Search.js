55fa03d8c0166b98b2bdcfcdf6b05492855c1ede
Jessica Seo <jessicaseo83@gmail.com>
2020-08-09 18:04:37 -0400

    Modify dropdown buttons and styles

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,8 +5,18 @@ import Map from "./Map/Map.js"
 import Button from 'react-bootstrap/Button'
 import "./Search.css"
 import UserInd from "./list/User.js"
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}))
 
 const Search = (props) => {
   const [users,setUsers] = useState([]);
@@ -45,7 +55,18 @@ const Search = (props) => {
   },[city,role])
 
 
-const userList = filteredUsers.map (user => <UserInd user={user}/>)
+const userList = filteredUsers.map (user => <UserInd key={user.id} user={user}/>)
+
+// dropdown button handlers
+const cityChange = (event) => {
+  setCity(event.target.value);
+};
+
+const roleChange = (event) => {
+  setRole(event.target.value);
+};
+
+const classes = useStyles();
 
 return (
 
@@ -53,24 +74,39 @@ return (
   
   <section className="sidebar"/>
   <section className="map_list">
-  <select id="role" className="search_filter" onChange={(event)=>setRole(event.target.value)} value ={role}>
-      <option value ="" selected  > Select a role</option>
-      <option>Musician</option>
-      <option>Sound Engineer</option>
-      <option>Songwriter</option>
-</select>
-
-
-<select id="city" className="search_filter" onChange={(event)=>setCity(event.target.value)} value ={city}>
-      <option value="" selected  > Select a city</option>
-      <option value="Montreal">Montreal</option>
-      <option value = "Toronto">Toronto</option>
-      <option value ="Vancouver">Vancouver</option>
-      <option value ="Ottawa"> Ottawa</option>
-</select>
+  
+<section>
+  <FormControl className={classes.formControl}>
+  <InputLabel id="cityfilter">Role</InputLabel>
+    <Select
+      labelId="rolefilter"
+      id="role"
+      value={role}
+      onChange={roleChange}
+    >
+      <MenuItem value="Musician">Musician</MenuItem>
+      <MenuItem value="Producer">Producer</MenuItem>
+      <MenuItem value="Songwriter">Songwriter</MenuItem>
+      <MenuItem value="Sound Engineer">Sound Engineer</MenuItem>
+    </Select>
+  </FormControl>
+  <FormControl className={classes.formControl}>
+    <InputLabel id="cityfilter">City</InputLabel>
+    <Select
+      labelId="cityfilter"
+      id="city"
+      value={city}
+      onChange={cityChange}
+    >
+      <MenuItem value="Montreal">Montreal</MenuItem>
+      <MenuItem value="Toronto">Toronto</MenuItem>
+      <MenuItem value="Ottawa">Vancouver</MenuItem>
+      <MenuItem value="Ottawa">Ottawa</MenuItem>
+    </Select>
+    
+  </FormControl>
 <Button className="filter-btn" type="button" onClick={clearFilters}> Clear All Filters</Button>
-
-
+</section>
 
 <form onChange={(event) => setView(event.target.value)}>
 
@@ -96,3 +132,18 @@ return (
 
 export default Search;
 
+{/* <select id="role" className="search_filter" onChange={(event)=>setRole(event.target.value)} value ={role}>
+      <option value ="" selected  > Select a role</option>
+      <option>Musician</option>
+      <option>Sound Engineer</option>
+      <option>Songwriter</option>
+</select>
+ */}
+
+{/* <select id="city" className="search_filter" onChange={(event)=>setCity(event.target.value)} value ={city}>
+      <option value="" selected  > Select a city</option>
+      <option value="Montreal">Montreal</option>
+      <option value = "Toronto">Toronto</option>
+      <option value ="Vancouver">Vancouver</option>
+      <option value ="Ottawa"> Ottawa</option>
+</select> */}
