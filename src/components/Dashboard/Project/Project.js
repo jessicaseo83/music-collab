692e38296b9d8f1724777d29c6326e7d492e5698
Audrey <audreymedaino@gmail.com>
2020-08-10 16:05:11 -0400

    Embed works

--- src/components/Dashboard/Project/Project.js
+++ src/components/Dashboard/Project/Project.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './Project.css';
-import axios from 'axios';
+import Row from "react-bootstrap/Row"
+import ReactPlayer from "react-player";
+
+
 
 export default function Project (props){
+
   const projects = props.project;
   console.log("These are the projects:", projects)
 
   return (
+    <>
+    <Row className="width">
     <div>
       <h3>My Projects</h3>
       {projects.map(project => (
@@ -14,10 +20,14 @@ export default function Project (props){
           <img className="project_pic" src={project.pic}/>
           <h4>{project.title}</h4>
           <p>{project.description}</p>
-          <a className="url" href={project.url}>Check here!</a>
+          <ReactPlayer url={project.url}/>
         </div>
       ))
       }
     </div>
+    </Row>
+    
+
+</>
   )
 }
