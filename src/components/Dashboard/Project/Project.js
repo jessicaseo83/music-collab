ab9a6b3735ccb4eb45af58e6e4a2ec20e5794dd2
Jessica Seo <jessicaseo83@gmail.com>
2020-08-12 21:19:36 -0400

    Implement flipbook

--- src/components/Dashboard/Project/Project.js
+++ src/components/Dashboard/Project/Project.js
@@ -2,29 +2,54 @@ import React from "react";
 import './Project.css';
 import Row from "react-bootstrap/Row"
 import ReactPlayer from "react-player";
-
+import FlipPage from "react-flip-page";
 
 
 export default function Project (props){
-
   const projects = props.project;
+  const pages = [
+  { title: "First chapter", content: "Content content content" },
+  { title: "Second chapter", content: "Content content content" },
+  { title: "Third chapter", content: "Content content content" }
+];
+
+
   console.log("These are the projects:", projects)
 
   return (
     <>
-    <Row className="width">
-    <div>
-      <h3>My Projects</h3>
-      {projects.map(project => (
+    <div className="width">
+    <div className="project-title">
+      <h2>My Projects</h2>
+      {/* {projects.map(project => (
         <div>
           <h4>{project.title}</h4>
           <p>{project.description}</p>
           <ReactPlayer className="player" url={project.url}/>
         </div>
       ))
-      }
+      } */}
     </div>
-    </Row>
+    <div className="app">
+      <FlipPage
+        className="book"
+        showSwipeHint
+        uncutPages
+        orientation="horizontal"
+        width="800"
+        pageBackground="#fffdf8"
+        animationDuration="400"
+      >
+        {projects.map(project => (
+          <article style={{ width: "300px ", padding: "10px 20px" }}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <ReactPlayer className="player" url={project.url}/>
+          </article>
+        ))}
+      </FlipPage>
+    </div>
+    </div>
     
 
 </>
